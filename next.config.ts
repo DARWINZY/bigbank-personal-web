93235db60927df9fff64bc6ed101d394f81db5f0
DARWINZY <bankntp8@gmail.com>
2026-06-16 15:16:24 +0700

    Restore basePath for default GitHub Pages URL

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,13 @@ import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+const repo = 'bigbank-personal-web';
+const basePath = isGithubActions ? `/${repo}` : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: basePath,
   images: {
     unoptimized: true,
   },
